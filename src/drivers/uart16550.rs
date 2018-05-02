eb1b450ef01730d3c3999178f4cdb2ed30fe79c3
Adrian Danis <rambobones@gmail.com>
2018-05-02 16:14:02 +1000

    theoretically functioning serial

--- src/drivers/uart16550.rs
+++ src/drivers/uart16550.rs
@@ -12,6 +12,11 @@ enum Parity {
     Even = 3,
     Mark = 5,
     Space = 7,
+    // Define additional invalid bit patterns so that all 3 bit patterns are represented
+    // This makes the transmute to convert from an integer to the enum less unsafe
+    Invalid0 = 2,
+    Invalid1 = 4,
+    Invalid2 = 6,
 }
 
 #[repr(u8)]
@@ -22,40 +27,33 @@ enum WordLength {
     Bits8 = 3,
 }
 
-impl BitRange<Parity> for u8 {
-    fn bit_range(&self, msb: usize, lsb: usize) -> Parity {
-        // This is a bit unsafe but we shall assume the hardware is
-        // correct here and that we never have invalid bit patterns
-        unsafe{mem::transmute((self as &BitRange<u8>).bit_range(msb, lsb))}
-    }
-    fn set_bit_range(&mut self, msb: usize, lsb: usize, value: Parity) {
-        (self as &mut BitRange<u8>).set_bit_range(msb, lsb, value as u8)
+impl From<u8> for WordLength {
+    fn from(value: u8) -> WordLength {
+        unsafe{mem::transmute(value)}
     }
 }
 
-impl BitRange<WordLength> for u8 {
-    fn bit_range(&self, msb: usize, lsb: usize) -> WordLength {
-        unsafe{mem::transmute((self as &BitRange<u8>).bit_range(msb, lsb))}
-    }
-    fn set_bit_range(&mut self, msb: usize, lsb: usize, value: WordLength) {
-        (self as &mut BitRange<u8>).set_bit_range(msb, lsb, value as u8)
+impl Into<u8> for WordLength {
+    fn into(self) -> u8 {
+        self as u8
     }
 }
 
-impl From<Parity> for u8 {
-    fn from(p: Parity) -> u8 {
-        p as u8
+impl From<u8> for Parity {
+    fn from(value: u8) -> Parity {
+        unsafe{mem::transmute(value)}
     }
 }
 
-impl From<WordLength> for u8 {
-    fn from(p: WordLength) -> u8 {
-        p as u8
+impl Into<u8> for Parity {
+    fn into(self) -> u8 {
+        self as u8
     }
 }
 
 bitfield!{
     pub struct LCR(u8);
+    no default BitRange;
     dlab, set_dlab: 7,7;
     sbe, set_sbe: 6,6;
     Parity, into Parity, parity, set_parity: 5, 3;
@@ -63,26 +61,15 @@ bitfield!{
     WordLength, into WordLength, word_len, set_word_len: 0, 2;
 }
 
-//bitflags! {
-    /// Line Control Register
-//    struct LCR: u8 {
-        /// Divisor Latch Access Bit
-//        const DLAB = 0b10000000;
-        /// Set Break Enable
-//        const SBE = 0b1000000;
-        /// Defines the set of bits that make up parity. You should always 
-        /// Space parity
-//        const P_SPACE = 0b111000;
-        /// Mark parity
-//        const P_MARK = 0b101000;
-        /// Even parity
-//        const P_EVEN = 0b011000;
-        /// Odd parity
-//        const P_ODD = 0b001000;
-        /// No parity
-//        const P_NONE = 0b000000;
-//    }
-//}
+impl<T> BitRange<T> for LCR where T: Into<u8>, T: From<u8> {
+    fn bit_range(&self, msb: usize, lsb: usize) -> T {
+        T::from((self as &BitRange<u8>).bit_range(msb, lsb))
+    }
+    fn set_bit_range(&mut self, msb: usize, lsb: usize, value: T) {
+        (self as &mut BitRange<u8>).set_bit_range(msb, lsb, value.into())
+    }
+}
+
 bitflags! {
     /// Interrupt Enable Register
     struct IER: u8 {
@@ -109,6 +96,48 @@ bitflags! {
     }
 }
 
+bitflags! {
+    /// Modem Control Register
+    struct MCR: u8 {
+        const RESERVED7 = 0b10000000;
+        const RESERVED6 = 0b01000000;
+        /// 16750 bit Autoflow Control Enabled
+        const ACE = 0b100000;
+        /// Loopback Mode
+        const LM = 0b10000;
+        /// Auxiliary Output 2
+        const AO2 = 0b1000;
+        /// Auxiliary Output 1
+        const AO1 = 0b100;
+        /// Request To Send
+        const RTS = 0b10;
+        /// Data Terminal Ready
+        const DTS = 0b1;
+    }
+}
+
+bitflags! {
+    /// Line Status Register
+    struct LSR: u8 {
+        /// Error in Received FIFO
+        const ERFIFO = 0b10000000;
+        /// Empty Data Holding Registers
+        const EDHR = 0b1000000;
+        /// Empty Transmitter Holding Registers
+        const ETHR = 0b100000;
+        /// Break INterrupt
+        const BI = 0b10000;
+        /// Framing Error
+        const FE = 0b1000;
+        /// Parity Error
+        const PE = 0b100;
+        /// Overrun Error
+        const OE = 0b10;
+        /// Data Read
+        const DR = 0b1;
+    }
+}
+
 /// Description of the registers of the serial port
 pub struct Uart<T: Io<Item = u8>> {
     /// Store the underlying IO accessor
@@ -125,7 +154,6 @@ impl<T, R> Uart<T> where T: Io<Item = u8, Range=R>, R: From<u8> {
         self.io.write(R::from(0), data)
     }
     unsafe fn read_lcr(&mut self) -> LCR {
-//        LCR::from_bits_truncate(self.io.read(R::from(3)))
         LCR(self.io.read(R::from(3)))
     }
     unsafe fn write_lcr(&mut self, lcr: LCR) {
@@ -137,21 +165,68 @@ impl<T, R> Uart<T> where T: Io<Item = u8, Range=R>, R: From<u8> {
     unsafe fn write_ier(&mut self, ier: IER) {
         self.io.write(R::from(1), ier.bits())
     }
+    unsafe fn read_mcr(&mut self) -> MCR {
+        MCR::from_bits_truncate(self.io.read(R::from(4)))
+    }
+    unsafe fn write_mcr(&mut self, mcr: MCR) {
+        self.io.write(R::from(4), mcr.bits())
+    }
+    unsafe fn read_lsr(&mut self) -> LSR {
+        LSR::from_bits_truncate(self.io.read(R::from(5)))
+    }
+    // We do not model the FIFO Control Register at all as we do not support
+    // doing anything with it
+    unsafe fn write_fifo_0(&mut self) {
+        self.io.write(R::from(2), 0)
+    }
     unsafe fn set_dlab(&mut self, state: bool) {
         let mut lcr = self.read_lcr();
         lcr.set_dlab(state as u8);
+        self.write_lcr(lcr);
+    }
+    unsafe fn set_break(&mut self, state: bool) {
+        let mut lcr = self.read_lcr();
+        lcr.set_sbe(state as u8);
         self.write_lcr(lcr);
     }
     unsafe fn disable_interrupts(&mut self) {
         let ier = self.read_ier();
         self.write_ier(ier - IER::ALL_INT)
     }
+    unsafe fn disable_fifo(&mut self) {
+        self.write_fifo_0()
+    }
+    unsafe fn configure_line(&mut self, bits: WordLength, two_stops: bool, parity: Parity) {
+        let mut lcr = self.read_lcr();
+        lcr.set_stops(two_stops as u8);
+        lcr.set_word_len(bits);
+        lcr.set_parity(parity);
+    }
+    unsafe fn write_latch(&mut self, latch: u16) {
+        self.set_dlab(true);
+        let high = (latch >> 8) as u8;
+        let low = (latch & 0xff) as u8;
+        self.io.write(R::from(1), high);
+        self.io.write(R::from(0), low);
+        self.set_dlab(false);
+    }
+    unsafe fn set_baud_rate(&mut self, baud: u32) {
+        let latch = (115200 / baud) as u16;
+        self.write_latch(latch);
+    }
     unsafe fn init(&mut self) {
         // Minimal attempt to create sane state by disabling interrupts, the fifo and
         // ensuring the dlab is in its default (off) position
         self.set_dlab(false);
         self.disable_interrupts();
-//        self.disable_fifo();
+        self.disable_fifo();
+        // TODO: Just go ahead and initialize it with some assumptions. Init should
+        // really be done through the generic serial interface
+        self.configure_line(WordLength::Bits8, false, Parity::None);
+        self.set_break(false);
+        self.set_baud_rate(115200);
+        let mcr = self.read_mcr();
+        self.write_mcr((mcr - MCR::ACE - MCR::LM) | MCR::AO2 | MCR::AO1 | MCR::RTS | MCR::DTS);
     }
     unsafe fn new(io: T) -> Uart<T> {
         let mut sp = Uart { io: io };
@@ -162,5 +237,7 @@ impl<T, R> Uart<T> where T: Io<Item = u8, Range=R>, R: From<u8> {
 
 impl<T, R> Serial for Uart<T> where T: Io<Item = u8, Range=R>, R: From<u8> {
     unsafe fn write_byte(&mut self, byte: u8) {
+        while !self.read_lsr().contains(LSR::ETHR) {}
+        self.write_data(byte);
     }
 }
