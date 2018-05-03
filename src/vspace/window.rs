4599193c6dc3871710b47e01fd3206627cd0e350
Adrian Danis <rambobones@gmail.com>
2018-05-03 20:33:17 +1000

    clone sufficient

--- src/vspace/window.rs
+++ src/vspace/window.rs
@@ -6,4 +6,6 @@
 // from the early window need some way to transfer to the final window, but this means
 // they need an address change. is there a way to do that? 'create' a new object
 // that happens to be the same as the original at a new address? Probably not, maybe
-// a Copy needs to be done, but how to do that in place?
+// a Copy needs to be done, but how to do that in place? I think Clone is sufficient
+// as it implies the object can be 'memcpy'd to duplicate, so should also be safe
+// to reinterpret at a new virtual address.
