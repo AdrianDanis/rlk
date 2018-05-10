d828541e68f0be047d723faa7beb74ebb3564640
Adrian Danis <rambobones@gmail.com>
2018-05-11 07:11:46 +1000

    Define as freemem list

--- src/heap/steal.rs
+++ src/heap/steal.rs
@@ -3,8 +3,32 @@
 use core::ops::Range;
 use alloc::linked_list::LinkedList;
 
-// All memory is in terms of virtual addresses
-static mut FREE_MEM: Option<LinkedList<Range<usize>>> = None;
-static mut USED_MEM: Option<LinkedList<Range<usize>>> = None;
-static mut BOOTSTRAP: [Range<usize>; 1] = [0..0];
+/// Structure for creating linked list of ranges in free memory (similar to a classic frame table)
+struct LinkedFreemem {
+    /// Virtual address range in the kernel window for this memory
+    ///
+    /// This instance of `LinkedFreemem` sits at the start of this range
+    range: [Range<usize>; 1],
+    next: Option<&'static mut LinkedFreemem>,
+    prev: Option<&'static mut LinkedFreemem>,
+}
 
+const MAX_USED_MEM: usize = 8;
+
+struct Heap {
+    /// Freemem list
+    free_mem: Option<&'static mut LinkedFreemem>,
+    /// Regions of memory that may already be in use and must never be added to the freemem list
+    used_mem: [Option<[Range<usize>;1]>; MAX_USED_MEM],
+}
+
+static mut HEAP: Heap = Heap { free_mem: None, used_mem: [None, None, None, None, None, None, None, None]};
+
+impl LinkedFreemem {
+}
+
+/// Initialize the steal memory allocator and enable it
+///
+/// Requires an initial block of free memory to get things going
+unsafe fn init() {
+}
