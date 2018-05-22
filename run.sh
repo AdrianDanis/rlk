8695ab73df70343a04e128ca7e701683f31ccdf0
Adrian Danis <rambobones@gmail.com>
2018-05-22 15:27:40 +1000

    Disable graphics for now

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 objcopy --output-target elf32-i386 $1 $1.elf32
 
-qemu-system-x86_64 -M pc -m 64 -kernel $1.elf32 -cpu Haswell,+pdpe1gb -serial mon:stdio -append "--earlycon=serial,port=0x3f8" -no-reboot
+qemu-system-x86_64 -M pc -m 64 -kernel $1.elf32 -cpu Haswell,+pdpe1gb -serial mon:stdio -nographic -append "--earlycon=serial,port=0x3f8" -no-reboot
