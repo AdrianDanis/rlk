b8aff6f5570d07c97948c3e974132d2e09774887
Adrian Danis <rambobones@gmail.com>
2018-06-07 14:29:00 +1000

    stack TODO

--- src/main.rs
+++ src/main.rs
@@ -61,6 +61,7 @@ pub extern "C" fn boot_system(arg1: usize, arg2: usize) -> ! {
     }
     print!(Info, "Switching to full kernel address space");
     unsafe {vspace::make_kernel_address_space()};
+    // TODO: switch to a new stack that is guarded and not in our boot memory
     print!(Info, "arg1 is {:x}", arg1);
     print!(Panic, "Panic");
     print!(Error, "Error");
