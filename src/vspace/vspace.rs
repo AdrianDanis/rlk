6947605639357b92150023654614e54fc524dd8e
Adrian Danis <rambobones@gmail.com>
2018-06-28 11:31:41 +1000

    document

--- src/vspace/vspace.rs
+++ src/vspace/vspace.rs
@@ -7,9 +7,10 @@ use cpu;
 
 pub unsafe fn make_kernel_address_space() {
     // create kernel address space
+    // TODO: nothing has guaranteed that our AS (read: PML4) is a page aligned allocation
     let kernel_as = Box::into_raw(box AS::default());
     (*kernel_as).map_kernel_window();
-    // inform any early cons that we are switching
+    // TODO: inform any early cons that we are switching
     // enable address space
     let kernel_as_paddr = KERNEL_WINDOW.vaddr_to_paddr(kernel_as as usize).unwrap();
     // Load CR3, this will invalidate all our translation information so there is nothing else
