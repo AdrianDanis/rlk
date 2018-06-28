c0c5a708f9b0bbcfa4a55ae5fbdb15aa545e40f7
Adrian Danis <rambobones@gmail.com>
2018-06-28 13:33:09 +1000

    move map_kernel_window

--- src/vspace/kernel.rs
+++ src/vspace/kernel.rs
@@ -1,16 +1,52 @@
 //! Implementation of final kernel address space
 
-use vspace::{AS, KERNEL_PCID};
-use state::KERNEL_WINDOW;
+use vspace::*;
+use vspace::paging::*;
+use cpu::features::Page1GB;
+use state::{CPU_FEATURES, KERNEL_WINDOW};
 use alloc::boxed::Box;
 use cpu;
 
+struct KernelVSpace(AS);
+
+impl Default for KernelVSpace {
+    fn default() -> Self {
+        KernelVSpace{0: AS::default()}
+    }
+}
+
+impl KernelVSpace {
+    unsafe fn map_kernel_window(&mut self) {
+        // currently assume 1gb pages
+        let page1gb: Page1GB = unsafe{CPU_FEATURES}.get_page1gb().expect("Require 1GB page support");
+        // create the guaranteed kernel mappings
+        for gb in KERNEL_BASE_DEFAULT_RANGE.step_by(GB) {
+            // as this is not the kernel image, no need for executable
+            let mapping = PageMappingBuilder::new(gb, gb - (KERNEL_BASE - KERNEL_PHYS_BASE), PageSize::Huge(page1gb)).kernel().no_execute().write().finish();
+            unsafe {
+                self.0.ensure_mapping_entry(mapping);
+                self.0.raw_map_page(mapping);
+            }
+        }
+        // map in the kernel image
+        for gb in KERNEL_IMAGE_RANGE.step_by(GB) {
+            // unfortunately the data and bss is also here so we need this both executable and writable
+            let mapping = PageMappingBuilder::new(gb, gb - (KERNEL_IMAGE_BASE - KERNEL_PHYS_BASE), PageSize::Huge(page1gb)).kernel().executable().write().finish();
+            unsafe {
+                self.0.ensure_mapping_entry(mapping);
+                self.0.raw_map_page(mapping);
+            }
+        }
+    }
+}
+
 pub unsafe fn make_kernel_address_space() {
     // create kernel address space
-    let kernel_as = Box::into_raw(box AS::default());
+    let kernel_as = Box::into_raw(box KernelVSpace::default());
     (*kernel_as).map_kernel_window();
     // TODO: inform any early cons that we are switching
     // enable address space
+    // TODO: pull out 'AS' portion of kernel_as
     let kernel_as_paddr = KERNEL_WINDOW.vaddr_to_paddr(kernel_as as usize).unwrap();
     // Load CR3, this will invalidate all our translation information so there is nothing else
     // we need to do
--- src/vspace/paging.rs
+++ src/vspace/paging.rs
@@ -8,13 +8,13 @@ use cpu::MemoryType;
 use alloc::boxed::Box;
 use core::mem;
 
-enum Rights {
+pub enum Rights {
     Read,
     ReadWrite,
 }
 
 #[derive(Debug, Clone, Copy)]
-enum PageSize {
+pub enum PageSize {
     // 4KiB
     Small,
     // 2MiB
@@ -73,7 +73,7 @@ impl Access {
 }
 
 #[derive(Debug, Clone, Copy)]
-struct PageMapping {
+pub struct PageMapping {
     vaddr: usize,
     paddr: usize,
     pge: Option<PGE>,
@@ -96,7 +96,7 @@ impl From<PageMapping> for PDPTEntry {
     }
 }
 
-struct PageMappingBuilder {
+pub struct PageMappingBuilder {
     internal: PageMapping,
 }
 
@@ -186,7 +186,7 @@ impl AS {
     /// # Panics
     ///
     /// If the desired virtual address is already marked as present then 
-    unsafe fn raw_map_page(&mut self, mapping: PageMapping) {
+    pub unsafe fn raw_map_page(&mut self, mapping: PageMapping) {
 
         let pml4ent = &self.0[pml4_index(VAddr::from_usize(mapping.vaddr))];
         let pdpt = PDPTWrap::from_entry(*pml4ent);
@@ -210,7 +210,7 @@ impl AS {
     /// Will panic if the entry cannot be created due to a frame existing at a higher level.
     /// i.e. if trying to ensure an entry for a 4K frame but there is already a 2M frame
     /// covering the region, preventing the necessary page table from being created.
-    unsafe fn ensure_mapping_entry(&mut self, mapping: PageMapping) {
+    pub unsafe fn ensure_mapping_entry(&mut self, mapping: PageMapping) {
         let pml4ent = &mut self.0[pml4_index(VAddr::from_usize(mapping.vaddr))];
         if !pml4ent.is_present() {
             let pdpt = box PDPTWrap::default();
@@ -222,28 +222,6 @@ impl AS {
             return;
         }
         unimplemented!()
-    }
-    pub unsafe fn map_kernel_window(&mut self) {
-        // currently assume 1gb pages
-        let page1gb: Page1GB = unsafe{CPU_FEATURES}.get_page1gb().expect("Require 1GB page support");
-        // create the guaranteed kernel mappings
-        for gb in KERNEL_BASE_DEFAULT_RANGE.step_by(GB) {
-            // as this is not the kernel image, no need for executable
-            let mapping = PageMappingBuilder::new(gb, gb - (KERNEL_BASE - KERNEL_PHYS_BASE), PageSize::Huge(page1gb)).kernel().no_execute().write().finish();
-            unsafe {
-                self.ensure_mapping_entry(mapping);
-                self.raw_map_page(mapping);
-            }
-        }
-        // map in the kernel image
-        for gb in KERNEL_IMAGE_RANGE.step_by(GB) {
-            // unfortunately the data and bss is also here so we need this both executable and writable
-            let mapping = PageMappingBuilder::new(gb, gb - (KERNEL_IMAGE_BASE - KERNEL_PHYS_BASE), PageSize::Huge(page1gb)).kernel().executable().write().finish();
-            unsafe {
-                self.ensure_mapping_entry(mapping);
-                self.raw_map_page(mapping);
-            }
-        }
     }
 }
 
